05980cd0eb044a608a211012abb3ad6cb2f73396
H-Senhaji <senhajihafsa92@gmail.com>
2019-10-29 11:28:44 +0100

    loginbackend

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const db = require('./db')
 const bodyParser = require('body-parser')
 const userRouter = require ('./user/router')
 const cors = require ('cors')
-
 const app = express();
 const port = process.env.PORT || 4000;
+const authRouter = require("./auth/router");
+
 
 //const User = require ('./user/model')
 
@@ -19,6 +20,8 @@ app
 .use(corsMiddleware)
 .use(jsonMiddleware)
 .use(userRouter)
+.use(authRouter)
+
 
 app
 .listen(port, () => console.log(`Example app listening on port ${port}!`));
