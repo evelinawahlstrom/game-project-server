d0028d252b781ba545f8f65fc677bb1ef6862753
Evelina <evelinawahlstrom18@gmail.com>
2019-10-30 17:12:31 +0100

    fixed-error

---
+++ game/model.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+
+const Game = db.define("game", {
+  first_player_id: {
+    type: Sequelize.INTEGER
+  },
+  second_player_id: {
+    type: Sequelize.INTEGER
+  },
+  game_id: {
+    type: Sequelize.INTEGER
+  },
+  game_status: {
+      type: Sequelize.STRING
+  },
+  turn: {
+     type: Sequelize.INTEGER
+  }
+})
+
+module.exports = Game; 
