9566b3ea21459d1f2e83d7efe319dff7ef2393f8
Evelina Wahlström <evelinawahlstrom18@gmail.com>
2019-10-30 16:00:36 +0100

    Revert "Game model"

--- index.js
+++ index.js
@@ -3,19 +3,12 @@ const express = require("express");
 const db = require('./db')
 const bodyParser = require('body-parser')
 const userRouter = require ('./user/router')
-///const gameRouter = require ('./game/router')
 const cors = require ('cors')
-const authRouter = require("./auth/router");
-const streamRouter = require('./stream/router')
-
 
 const app = express();
 const port = process.env.PORT || 4000;
 
-
-
 //const User = require ('./user/model')
-const Game = require ('./game/model')
 
 // middleware
 const jsonMiddleware = bodyParser.json()
@@ -25,16 +18,11 @@ const corsMiddleware = cors()
 app
 .use(corsMiddleware)
 .use(jsonMiddleware)
-.use(streamRouter)
 .use(userRouter)
-.use(Game)
-.use(authRouter)
-
 
 app
 .listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-// db - don't forget to put force: false, when finished with development
 db
 .sync({force: true})
 .then(() => {
